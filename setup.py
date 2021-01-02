3276dfdba4008ca4e1f6def84ccdac3773d871f0
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 18:32:10 +0300

    Updated url in setup.py.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='1.0.0',
     packages=['PosTagger'],
     package_data={'PosTagger': ['*.pxd', '*.pyx', '*.c']},
-    url='https://github.com/olcaytaner/PosTagger-Cy',
+    url='https://github.com/StarlangSoftware/PosTagger-Cy',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
