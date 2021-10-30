9a96c969931ec9c3394c24438fd51dd1698e3d47
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 21:43:12 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(["PosTagger/*.pyx"],
                           compiler_directives={'language_level': "3"}),
     name='NlpToolkit-PosTagger-Cy',
-    version='1.0.0',
+    version='1.0.1',
     packages=['PosTagger'],
     package_data={'PosTagger': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/PosTagger-Cy',
@@ -13,5 +17,7 @@ setup(
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='English PosTagger Library',
-    install_requires=['NlpToolkit-Corpus-Cy', 'NlpToolkit-Hmm-Cy']
+    install_requires=['NlpToolkit-Corpus-Cy', 'NlpToolkit-Hmm-Cy'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
