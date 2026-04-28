43f25f05e42a6dc75c8d6a2480d66f030e0e797c
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2026-04-28 20:35:21 +0300

    Updated name and version.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["PosTagger/*.pyx"],
                           compiler_directives={'language_level': "3"}),
-    name='nlptoolkit-postagger-cy',
-    version='1.0.5',
+    name='nlptoolkit_postagger_cy',
+    version='1.0.6',
     packages=['PosTagger'],
     package_data={'PosTagger': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/StarlangSoftware/PosTagger-Cy',
